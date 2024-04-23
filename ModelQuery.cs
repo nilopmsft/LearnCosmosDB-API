d437f9eb7ac31e5cc6e26827246d775fe6f8144c
Nick Lopez <nilop@microsoft.com>
2024-04-23 17:07:02 -0500

    kind of working, no results to insomnia

--- ModelQuery.cs
+++ ModelQuery.cs
@@ -5,6 +5,7 @@ using Microsoft.Extensions.Logging;
 using Microsoft.Azure.Cosmos;
 using System.Text.Json;
 using Newtonsoft.Json.Linq;
+using Newtonsoft.Json;
 
 
 namespace Modules.Modeling
@@ -12,16 +13,114 @@ namespace Modules.Modeling
 
     public class RequestValues
     {
+        public string SearchValue { get; set; }
         public int? Id { get; set; }
-        public string? Title { get; set; }
-        public string? ModelContainer { get; set; }
-        public string? SearchType { get; set; }
+        public string? type { get; set; }
     }
-    public class ModelSingleDocument
+
+    public class QueryCosmos
+    {
+        private readonly ILogger<QueryCosmos> _logger;
+        private readonly CosmosClient _cosmosClient;
+        public string? SearchType { get; set; }
+
+        public QueryCosmos(ILogger<QueryCosmos> logger, CosmosClient cosmosClient)
+        {
+            _logger = logger;
+            _cosmosClient = cosmosClient;
+        }
+
+
+        [Function("QueryCosmos")]
+        public async Task<IActionResult> RunAsync([HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = "module/modeling/{container}")] HttpRequest req, string container)
+        {
+            _logger.LogInformation("C# HTTP trigger function processed a request.");
+
+            string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
+            RequestValues RequestValues = System.Text.Json.JsonSerializer.Deserialize<RequestValues>(requestBody);
+
+            Container cosmosContainer  = _cosmosClient.GetContainer(Environment.GetEnvironmentVariable("CosmosDbDatabaseName"), container);
+
+            // if (requestValues.Id != null && requestValues.Title != null)
+            // {
+
+            //     return new BadRequestObjectResult("We would do a point read on id: " + requestValues.Id + " and a partition key " + requestValues.Title + "for model " + requestValues.ModelContainer);
+
+            // }
+            // else
+            // {
+                // if (requestValues.SearchType == null)
+                // {
+                //     string sqlQueryText = $"SELECT * FROM c WHERE c.title = '{requestValues.Title}'";
+                // }
+                // else
+                // {
+                //     string sqlQueryText = $"SELECT * FROM c WHERE c.title = '{requestValues.Title}'";
+                // }
+                string SearchValue = RequestValues.SearchValue.ToLower();
+                string sqlQueryText = $"SELECT * FROM c WHERE c.title = '{SearchValue}'";
+                // _logger.LogInformation("Executing query: {0}\n", sqlQueryText);
+
+                FeedIterator<Object> queryResultSetIterator = cosmosContainer.GetItemQueryIterator<Object>(sqlQueryText);
+                QueryResult QueryResult = new QueryResult();
+
+                // Adding request into to the diagnostic result
+                QueryResult.QueryDiagnostics.QueryText = sqlQueryText;
+                QueryResult.QueryDiagnostics.SearchValue = SearchValue;
+                
+
+                while (queryResultSetIterator.HasMoreResults)
+                {
+                    //Check for the document type and deserialize it into the appropriate class
+                    FeedResponse<Object> currentResultSet = await queryResultSetIterator.ReadNextAsync();
+                    foreach (Object item in currentResultSet)
+                    {
+                        JObject jObject = JObject.FromObject(item);
+                        string itemtype = jObject["type"].ToString();
+                        string itemModel = "Media" + container + char.ToUpper(itemtype[0]) + itemtype.Substring(1);
+                        _logger.LogInformation("Type: {0}", itemtype);
+                        _logger.LogInformation("Model: {0}", itemModel);
+
+                        object MediaItem;
+                        switch (itemModel)
+                        {
+                            case "MediaEmbeddedPerson":
+                                MediaItem = JsonConvert.DeserializeObject<MediaEmbeddedPerson>(jObject.ToString());
+                                break;
+                            case "MediaReferencePerson":
+                                MediaItem = JsonConvert.DeserializeObject<MediaReferencePerson>(jObject.ToString());
+                                break;
+                            case "MediaHybridPerson":
+                                MediaItem = JsonConvert.DeserializeObject<MediaHybridPerson>(jObject.ToString());
+                                break;
+                            default: // Default to single model for media as it can be used for all types in that container
+                                MediaItem = JsonConvert.DeserializeObject<MediaSingle>(jObject.ToString());
+                                break;
+                        }
+
+                        _logger.LogInformation("Item is {0}", MediaItem);
+                        _logger.LogInformation("Item document type {0}", itemModel);
+
+                        //QueryResult.MediaResults.Add(MediaItem);
+
+                    }
+
+                    QueryResult.QueryDiagnostics.RequestCharge = currentResultSet.RequestCharge.ToString();
+                    QueryResult.QueryDiagnostics.ActivityId = currentResultSet.ActivityId;
+                }
+                _logger.LogInformation("Query Result: {0}", QueryResult);
+                
+                return new OkObjectResult(QueryResult);
+            }
+        }
+    }
+
+    public class MediaSingle
     {
         public string id { get; set; }
         public string type { get; set; }
         public string title { get; set; }
+        public string original_title { get; set; }
         public string tagline { get; set; }
         public string description { get; set; }
         public string mpaa_rating { get; set; }
@@ -30,6 +129,63 @@ namespace Modules.Modeling
         public Genre[] genres { get; set; }
         public Actor[] actors { get; set; }
         public Director[] directors { get; set; }
+        public string _rid { get; set; }
+        public string _self { get; set; }
+        public string _etag { get; set; }
+        public string _attachments { get; set; }
+        public int _ts { get; set; }
+    }
+
+    public class MediaEmbeddedPerson
+    {
+        public string id { get; set; }
+        public string type { get; set; }
+        public string title { get; set; }
+        public string original_title { get; set; }
+        public string movie_id { get; set; }
+        public string movie_title { get; set; }
+        public string tagline { get; set; }
+        public string description { get; set; }
+        public string mpaa_rating { get; set; }
+        public string release_date { get; set; }
+        public string poster_url { get; set; }
+        public Genre[] genres { get; set; }
+        public Actor[] actors { get; set; }
+        public Director[] directors { get; set; }
+        public string _rid { get; set; }
+        public string _self { get; set; }
+        public string _etag { get; set; }
+        public string _attachments { get; set; }
+        public int _ts { get; set; }
+    }
+
+    public class MediaReferencePerson
+    {
+        public string id { get; set; }
+        public string type { get; set; }
+        public string title { get; set; }
+        public string original_title { get; set; }
+        public string movie_id { get; set; }
+        public string movie_title { get; set; }
+        public string tagline { get; set; }
+        public string description { get; set; }
+        public string mpaa_rating { get; set; }
+        public string release_date { get; set; }
+        public string poster_url { get; set; }
+        public string _rid { get; set; }
+        public string _self { get; set; }
+        public string _etag { get; set; }
+        public string _attachments { get; set; }
+        public int _ts { get; set; }
+    }
+
+    public class MediaHybridPerson
+    {
+        public string id { get; set; }
+        public string title { get; set; }
+        public string original_title { get; set; }
+        public string type { get; set; }
+        public Role[] roles { get; set; }
         public string _rid { get; set; }
         public string _self { get; set; }
         public string _etag { get; set; }
@@ -54,107 +210,27 @@ namespace Modules.Modeling
         public string name { get; set; }
         public string id { get; set; }
     }
-    public class CosmosDbService
+
+    public class Role
     {
-        public Container _container;
-
-        public CosmosDbService(
-            string endpointUrl,
-            string primaryKey,
-            string databaseName,
-            string containerName)
-        {
-            CosmosClient client = new CosmosClient(endpointUrl, primaryKey);
-            _container = client.GetContainer(databaseName, containerName);
-        }
-        // public class CosmosDbService
-        // {
-        //     public Container _container;
-
-        //     public CosmosDbService(
-        //         CosmosClient dbClient,
-        //         string databaseName,
-        //         string containerName)
-        //     {
-        //         _container = dbClient.GetContainer(databaseName, containerName);
-        //     }
-
-        public async Task<ItemResponse<dynamic>> GetItemAsync(string id, string title)
-        {
-            try
-            {
-                ItemResponse<dynamic> response = await _container.ReadItemAsync<dynamic>(id, new PartitionKey(title));
-                return response;
-            }
-            catch (CosmosException ex) when (ex.StatusCode == System.Net.HttpStatusCode.NotFound)
-            {
-                return null;
-            }
-        }
-        // }
+        public string movie_id { get; set; }
+        public string movie_title { get; set; }
+        public string mpaa_rating { get; set; }
+        public string release_date { get; set; }
+        public string role { get; set; }
     }
 
-    public class QueryModel
+    public class QueryResult
     {
-        private readonly ILogger<QueryModel> _logger;
-
-        public QueryModel(ILogger<QueryModel> logger)
-        {
-            _logger = logger;
-        }
-
-        [Function("QueryModel")]
-        public async Task<IActionResult> RunAsync([HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = "module/modeling")] HttpRequest req)
-        {
-            _logger.LogInformation("C# HTTP trigger function processed a request.");
-
-            string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
-            RequestValues requestValues = JsonSerializer.Deserialize<RequestValues>(requestBody);
-
-            CosmosDbService cosmosDbService = new CosmosDbService(
-                Environment.GetEnvironmentVariable("CosmosDbEndpointUrl"),
-                Environment.GetEnvironmentVariable("CosmosDbPrimaryKey"),
-                Environment.GetEnvironmentVariable("CosmosDbDatabaseName"),
-                requestValues.ModelContainer
-            );
-
-            if (requestValues.Id != null && requestValues.Title != null)
-            {
-
-                return new BadRequestObjectResult("We would do a point read on id: " + requestValues.Id + " and a partition key " + requestValues.Title + "for model " + requestValues.ModelContainer);
-
-            }
-            else
-            {
-                // if (requestValues.SearchType == null)
-                // {
-                //     string sqlQueryText = $"SELECT * FROM c WHERE c.title = '{requestValues.Title}'";
-                // }
-                // else
-                // {
-                //     string sqlQueryText = $"SELECT * FROM c WHERE c.title = '{requestValues.Title}'";
-                // }
-                string sqlQueryText = $"SELECT * FROM c WHERE c.title = '{requestValues.Title}'";
-                _logger.LogInformation("Executing query: {0}\n", sqlQueryText);
-                var queryDefinition = new QueryDefinition(sqlQueryText);
-                FeedIterator<dynamic> queryResultSetIterator = cosmosDbService._container.GetItemQueryIterator<dynamic>(queryDefinition);
-                // Run the SQL query against Cosmos DB
-                List<dynamic> results = new List<dynamic>();
-                while (queryResultSetIterator.HasMoreResults)
-                {
-                    FeedResponse<dynamic> currentResultSet = await queryResultSetIterator.ReadNextAsync();
-                    foreach (var item in currentResultSet)
-                    {
-                        JObject jObject = JObject.FromObject(item);
-                        results.Add(jObject);
-                        //results.Add(item);
-                        Console.WriteLine($"Found item:\t{item}");
-                    }
-                }
-
-                return new OkObjectResult(results);
-            }
-
-        }
+        //public List<dynamic> MediaResults = new List<dynamic>();
+        public QueryDiagnostics QueryDiagnostics = new QueryDiagnostics();
     }
-}
+
+    public class QueryDiagnostics
+    {
+        public string QueryText { get; set; }
+        public string SearchValue { get; set; }
+        public string RequestCharge { get; set; }
+        public string ActivityId { get; set; }
+    }
+
--- Program.cs
+++ Program.cs
@@ -1,12 +1,19 @@
 using Microsoft.Azure.Functions.Worker;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Azure;
+using Microsoft.Azure.Cosmos;
 
 var host = new HostBuilder()
     .ConfigureFunctionsWebApplication()
     .ConfigureServices(services => {
         services.AddApplicationInsightsTelemetryWorkerService();
         services.ConfigureFunctionsApplicationInsights();
+        services.AddSingleton<CosmosClient>(serviceProvider =>
+        {
+            return new CosmosClient(Environment.GetEnvironmentVariable("CosmosDbEndpointUrl"),Environment.GetEnvironmentVariable("CosmosDbPrimaryKey"));
+        });
+
     })
     .Build();
 
